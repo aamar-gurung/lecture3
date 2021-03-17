f9236ce6e0d8499954a69acfd816801e7d5425af
AmarGurung <aamar.grg@gmail.com>
2021-03-17 12:53:43 +1100

    Update script.js

--- example2/script.js
+++ example2/script.js
@@ -163,7 +163,7 @@ function init() {
 
     // create a scene and a camera
     scene = new THREE.Scene()
-    scene.background = new THREE.Color(0, 0, 0)
+    scene.background = new THREE.Color(1, 1, 1)
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
     camera.position.z = - 30
 
